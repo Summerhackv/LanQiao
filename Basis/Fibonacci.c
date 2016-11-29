49ac018e62aed6b9c88be8639c66b3fe657d824a
Summerhackv <Summerhackv@163.com>
2016-11-29 19:54:11 +0800

    First commit

--- Basis/Fibonacci.c
+++ Basis/Fibonacci.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <malloc.h>
 #define NUM 10007
-#define MAX 10000
+#define MAX 1000000
 
 int main (void)
 {
@@ -14,19 +15,27 @@ int main (void)
 		printf("The input is error\n");
 		exit(-1);
 	}	 
-	int Fibonacci[MAX];
-	Fibonacci[0]=1;
+	int *Fibonacci=(int *)malloc(1000000*sizeof(int)); 
 	Fibonacci[1]=1;
-	for (i=2;i<MAX;i++)
+	Fibonacci[2]=1;
+	if (n<=2)
 	{
-		Fibonacci[i]=Fibonacci[i-1]+Fibonacci[i-2];
-		if(n==i+1)
+		remainder=Fibonacci[n]%NUM;
+		printf("The remainder is %d\n ",remainder);
+	}
+	else
+	{
+		for (i=2;i<=MAX;i++)
 		{
-			remainder=Fibonacci[n-1]%NUM;
-			printf("The remainder is %d\n ",remainder);
-			exit(0);
-		}
+			Fibonacci[i]=Fibonacci[i-1]+Fibonacci[i-2];
+			if(n==i)
+			{
+				remainder=Fibonacci[n]%NUM;
+				printf("The remainder is %d\n ",remainder);
+				exit(0);
+			}
 			
+		}
 	}
 	return 0;
 }
