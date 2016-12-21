1485750852e3f3c7abd37817fb6aa888e505ee22
Summerhackv <Summerhackv@163.com>
2016-12-21 16:58:18 +0800

    New start

---
+++ Sort/Bubble_Sort+.c
@@ -0,0 +1,35 @@
+/*Basic Sort*/
+ #include <stdio.h>
+ 
+ int main (void)
+ {
+	/*Initialization*/
+	int N;
+	int Midd;
+	int i,j,k;
+	scanf ("%d",&N);
+ 	
+ 	/*Input*/
+ 	int Num[N];
+ 	for (k=0;k<N;k++)
+ 		scanf("%d",&Num[k]);
+ 	
+	/*Sort*/	
+ 	for (i=0;i<N;i++)
+ 	{
+ 		for (j=i+1;j<N;j++)
+ 		{
+ 			if(Num[i]>Num[j])
+ 			{
+ 				Midd=Num[i];
+ 				Num[i]=Num[j];
+ 				Num[j]=Midd;
+			}
+		}
+	}
+	
+	/*Output*/
+	for (k=0;k<N;k++)
+		printf("%d ",Num[k]);
+	return 0;
+ }
